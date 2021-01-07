3fa5d663770acffc04567d425a1b56fabfc7ecee
Gautier de Bruijne <gautier.debruijne.y7024@student.hogent.be>
2021-01-07 21:25:15 +0100

    Startup.cs; - added required services and its packages - added first data classes

---
+++ TravelApp_G15_API/Data/DataInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelApp_G15_API.Data
+{
+    public class DataInitializer
+    {
+        private readonly DatabaseContext _context;
+
+        public DataInitializer(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task InitalizeData()
+        {
+            _context.Database.EnsureDeleted();
+            if (_context.Database.EnsureCreated())
+            {
+
+            }
+        }
+    }
+}
--- TravelApp_G15_API/Startup.cs
+++ TravelApp_G15_API/Startup.cs
@@ -1,15 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using NSwag;
+using NSwag.Generation.Processors.Security;
+using System;
+using System.Linq;
+using System.Text;
+using TravelApp_G15_API.Data;
 
 namespace TravelApp_G15_API
 {
@@ -26,10 +29,70 @@ namespace TravelApp_G15_API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<DatabaseContext>(c => c.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentity<IdentityUser, IdentityRole>(r => r.User.RequireUniqueEmail = true).AddEntityFrameworkStores<DatabaseContext>();
+
+            services.AddScoped<DataInitializer>();
+
+            services.Configure<IdentityOptions>(options => {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 7;
+                options.Password.RequiredUniqueChars = 0;
+
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.AddOpenApiDocument(d =>
+            {
+                d.DocumentName = "OpenAPI 3";
+                d.Title = "Essentials API";
+                d.Version = "version 0.1";
+                d.Description = "The Essentials API makes our lives easier!";
+                d.AddSecurity("JWT", Enumerable.Empty<String>(), new OpenApiSecurityScheme
+                {
+                    Type = OpenApiSecuritySchemeType.ApiKey,
+                    Name = "Authorization",
+                    In = OpenApiSecurityApiKeyLocation.Header,
+                    Description = "Enter: \"Bearer <JWT Token>\""
+                });
+
+                d.OperationProcessors.Add(new AspNetCoreOperationSecurityScopeProcessor("JWT"));
+            });
+
+            services.AddAuthentication(a =>
+            {
+                a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(b =>
+                {
+                    b.RequireHttpsMetadata = false;
+                    b.SaveToken = true;
+                    b.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(Configuration["Tokens:Key"])),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        RequireExpirationTime = true
+                    };
+                });
+
+            services.AddCors(options => options.AddPolicy("AllowAllOrigins", builder => builder.AllowAnyOrigin()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DataInitializer init)
         {
             if (env.IsDevelopment())
             {
@@ -37,15 +100,21 @@ namespace TravelApp_G15_API
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseAuthentication();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+            app.UseCors("AllowAllOrigins");
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            init.InitalizeData().Wait();
         }
     }
 }
