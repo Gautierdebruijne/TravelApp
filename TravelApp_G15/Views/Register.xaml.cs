3b3b5534678d8ab9ce20a0b544e923602f5cbb56
Gautier de Bruijne <gautier.debruijne.y7024@student.hogent.be>
2021-01-13 22:58:47 +0100

    Add register validation & functionality

--- TravelApp_G15/Views/Register.xaml.cs
+++ TravelApp_G15/Views/Register.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
+using TravelApp_G15.ViewModels;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -30,6 +32,89 @@ namespace TravelApp_G15.Views
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(Login));
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+            ValidateLogin();
+            RegisterUser();
+        }
+
+        private void txtPasswordConfirm_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            ValidatePassword();
+        }
+
+        private void ValidateLogin()
+        {
+            if (txtEmail.Text != "" && txtEmail.Text != null && txtName.Text != "" && txtName.Text != null &&
+                txtPassword.Password != "" && txtPassword.Password != null && txtPasswordConfirm.Password != "" && txtPasswordConfirm.Password != null)
+            {
+                txtError.Text = "";
+
+                if (Regex.IsMatch(txtEmail.Text, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,})+)$"))
+                {
+                    if (Regex.IsMatch(txtPassword.Password, @"^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$"))
+                    {
+                        txtError.Text = "A password must have a minimum of seven characters, \nat least one uppercase letter, one lowercase letter, \none number and one special character";
+                    }
+                    else
+                    {
+                        if (txtPassword.Password == txtPasswordConfirm.Password)
+                            txtError.Text = "";
+                        else
+                            txtError.Text = "The given passwords do not match!";
+                    }
+                }
+                else
+                {
+                    txtError.Text = "The given email is incorrect!";
+                }
+            }
+            else
+            {
+                txtError.Text = "All fields are required!";
+            }
+        }
+
+        private void ValidatePassword()
+        {
+            txtError.Text = "";
+
+            if (Regex.IsMatch(txtPassword.Password, @"^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$"))
+            {
+                txtError.Text = "A password must have a minimum of seven characters, \nat least one uppercase letter, one lowercase letter, \none number and one special character";
+            }
+            else
+            {
+                if (txtPassword.Password == txtPasswordConfirm.Password)
+                    txtError.Text = "";
+                else
+                    txtError.Text = "The given passwords do not match!";
+            }
+        }
+
+        private async void RegisterUser()
+        {
+            var registervm = new RegisterViewModel();
+
+            try
+            {
+                await registervm.Register(txtName.Text, txtEmail.Text, txtPassword.Password, txtPasswordConfirm.Password);
+
+                if (registervm.Success)
+                {
+                    this.Frame.Navigate(typeof(Dashboard));
+                }
+                else
+                {
+                    txtError.Text = "This email has already been used!";
+                }
+            }
+            catch(Exception e)
+            {
+                txtError.Text = e.Message.ToString();
+            }
         }
     }
 }
