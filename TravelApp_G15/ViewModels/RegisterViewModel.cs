1e84af92d7004c50bb0a6eb09d18a642b958f645
Louis De Smeytere <louis.desmeytere@student.hogent.be>
2021-01-16 16:11:39 +0100

    register bug fix

--- TravelApp_G15/ViewModels/RegisterViewModel.cs
+++ TravelApp_G15/ViewModels/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,7 @@ namespace TravelApp_G15.ViewModels
     {
         public bool Success { get; set; } = false;
 
-        public async Task<String> Register(string name, string email, string password, string passwordconfirmation)
+        public async Task<String> Register(string name, string email, string password)
         {
             var clientHandler = new HttpClientHandler
             {
@@ -21,11 +22,14 @@ namespace TravelApp_G15.ViewModels
             };
             var client = new HttpClient(clientHandler);
 
-            var register = new Register(name, email, password, passwordconfirmation);
+            var register = new Register(name, email, password);
             var jsonRegister = JsonConvert.SerializeObject(register);
             var url = "https://localhost:5001/api/User/Register";
             //var url = "https://travelappg15api.azurewebsites.net/api/User/Register";
             var data = new StringContent(jsonRegister, Encoding.UTF8, "application/json");
+            string content = await data.ReadAsStringAsync();
+            Debug.WriteLine(content);
+            Debug.WriteLine("Ik geraak tot hier");
             var res = await client.PostAsync(url, data);
             var json = await res.Content.ReadAsStringAsync();
 
@@ -39,7 +43,7 @@ namespace TravelApp_G15.ViewModels
             {
                 Success = false;
             }
-
+            Debug.WriteLine("Ik geraak zelf tot hier");
             return res.Content.ToString();
         }
     }
